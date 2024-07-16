b43107f83d233629fe30c65a62495b35436144fc
ogous <oguzxan.ali@gmail.com>
2024-07-16 11:52:01 +0300

    chore: init playwright

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"export",
+    images:{
+        unoptimized:true
+    },
+    webpack(config) {
+        if (process.env.NODE_V8_COVERAGE) {
+          Object.defineProperty(config, "devtool", {
+            get() {
+              return "source-map";
+            },
+            set() {},
+          });
+        }
+
+        return config;
+    }  
+};
 
 export default nextConfig;
