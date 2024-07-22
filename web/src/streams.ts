58df2d0ded4a02d0429e634eaa1eee2a2f876b14
ogous <oguzxan.ali@gmail.com>
2024-07-22 12:37:05 +0300

    fix: use https stream urls

--- web/src/streams.ts
+++ web/src/streams.ts
@@ -2,10 +2,12 @@ export const streams = [
   {
     name: "Happy Canteen",
     altName: "快乐大食堂",
-    front: "http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8",
-    above: "http://streetcatpull.hellobike.com/live/4258783365322591678_1.m3u8",
+    front:
+      "https://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8",
+    above:
+      "https://streetcatpull.hellobike.com/live/4258783365322591678_1.m3u8",
     behind:
-      "http://streetcatpull.hellobike.com/live/4258783365322591678_2.m3u8",
+      "https://streetcatpull.hellobike.com/live/4258783365322591678_2.m3u8",
     countryCode: "au",
     charity: "RSPCA Australia",
     charityUrl: "https://www.rspca.org.au/donate",
@@ -14,10 +16,12 @@ export const streams = [
   {
     name: "Lucky 7-Eleven II",
     altName: "好运7-11-II",
-    front: "http://streetcatpull.hellobike.com/live/4291094747934800009_0.m3u8",
-    above: "http://streetcatpull.hellobike.com/live/4291094747934800009_1.m3u8",
+    front:
+      "https://streetcatpull.hellobike.com/live/4291094747934800009_0.m3u8",
+    above:
+      "https://streetcatpull.hellobike.com/live/4291094747934800009_1.m3u8",
     behind:
-      "http://streetcatpull.hellobike.com/live/4291094747934800009_2.m3u8",
+      "https://streetcatpull.hellobike.com/live/4291094747934800009_2.m3u8",
     countryCode: "cn",
     charity: "RSPCA Australia",
     charityUrl: "https://www.rspca.org.au/donate",
@@ -26,10 +30,12 @@ export const streams = [
   {
     name: "Auspicious Restaurant",
     altName: "吉祥食府",
-    front: "http://streetcatpull.hellobike.com/live/4412424173050749216_0.m3u8",
-    above: "http://streetcatpull.hellobike.com/live/4412424173050749216_1.m3u8",
+    front:
+      "https://streetcatpull.hellobike.com/live/4412424173050749216_0.m3u8",
+    above:
+      "https://streetcatpull.hellobike.com/live/4412424173050749216_1.m3u8",
     behind:
-      "http://streetcatpull.hellobike.com/live/4412424173050749216_2.m3u8",
+      "https://streetcatpull.hellobike.com/live/4412424173050749216_2.m3u8",
     countryCode: "jp",
     charity: "RSPCA Australia",
     charityUrl: "https://www.rspca.org.au/donate",
@@ -38,10 +44,12 @@ export const streams = [
   {
     name: "Mr. Fall",
     altName: "快乐猫屋2号",
-    front: "http://streetcatpull.hellobike.com/live/4255656354246951265_0.m3u8",
-    above: "http://streetcatpull.hellobike.com/live/4255656354246951265_1.m3u8",
+    front:
+      "https://streetcatpull.hellobike.com/live/4255656354246951265_0.m3u8",
+    above:
+      "https://streetcatpull.hellobike.com/live/4255656354246951265_1.m3u8",
     behind:
-      "http://streetcatpull.hellobike.com/live/4255656354246951265_2.m3u8",
+      "https://streetcatpull.hellobike.com/live/4255656354246951265_2.m3u8",
     countryCode: "cn",
     charity: "RSPCA Australia",
     charityUrl: "https://www.rspca.org.au/donate",
