2c27c735d0a6ce7322fb6ba4e538597c83a52ead
ogous <oguzxan.ali@gmail.com>
2024-07-30 22:19:56 +0300

    feat: guard donation for account states

--- web/src/components/buttons/donateButton.tsx
+++ web/src/components/buttons/donateButton.tsx
@@ -2,29 +2,55 @@
 import { thirdwebClient } from "@/providers/thirdwebClient";
 import { useStreamStore } from "@/stores/streamStore";
 import { useUserStore } from "@/stores/userStore";
-import { useActiveAccount, useConnectModal } from "thirdweb/react";
-
+import clsx from "clsx";
+import { useState } from "react";
+import {
+  useActiveAccount,
+  useConnectModal,
+  useIsAutoConnecting,
+} from "thirdweb/react";
+import { useCoolMode } from "@/hooks/useCoolMode";
+import FavedIcon from "#/images/icons/faved.svg";
+import Loader from "../loader";
 export default function DonateButton() {
   const account = useActiveAccount();
-  const { connect, isConnecting } = useConnectModal();
+  const { connect, isConnecting: isConnectingModal } = useConnectModal();
+  const isAutoConnecting = useIsAutoConnecting();
   const selectedStream = useStreamStore((s) => s.selectedStream);
   const incrementDonationClicks = useUserStore(
     (s) => s.incrementDonationClicks,
   );
+  const [isClicked, setIsClicked] = useState(false);
+  const isConnecting = isConnectingModal || isAutoConnecting;
   const handleClick = () => {
     if (account) {
+      setIsClicked(true);
       incrementDonationClicks(selectedStream.hash);
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 100);
     } else {
       connect({ client: thirdwebClient });
     }
   };
+  const coolRef = useCoolMode(FavedIcon.src, isConnecting || !account);
+
   return (
     <button
+      ref={coolRef}
       onClick={handleClick}
       disabled={isConnecting}
-      className="inline-flex h-[69px] shrink grow basis-0 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg bg-neutral-100 px-5 py-[25px]"
+      className={clsx(
+        isConnecting && "pointer-events-none",
+        isClicked && "hover:scale-90",
+        "relative inline-flex h-[69px] shrink grow basis-0 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg bg-neutral-100 px-5 py-[25px] transition-transform duration-300 hover:scale-110",
+      )}
     >
-      <div className="text-xl font-bold text-stone-950">❤️ Donate!</div>
+      {isConnecting ? (
+        <Loader />
+      ) : (
+        <div className="text-xl font-bold text-stone-950">❤️ Donate!</div>
+      )}
     </button>
   );
 }
