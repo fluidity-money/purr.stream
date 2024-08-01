7aebfc4e4cfa2dab2488ce1079970548e7a987f7
ogous <oguzxan.ali@gmail.com>
2024-08-01 14:35:38 +0300

    fix: syntax

--- web/src/providers/permalink.tsx
+++ web/src/providers/permalink.tsx
@@ -7,8 +7,9 @@ export default function PermalinkController() {
   const selectStream = useStreamStore((s) => s.selectStream);
   const searchParams = useSearchParams();
   const cat = searchParams.get("cat");
-  const camera =
-    (searchParams.get("camera") as (typeof streamCameras)[number]) || null;
+  const camera = searchParams.get("camera") as
+    | (typeof streamCameras)[number]
+    | null;
   const isCatValid = cat && streams.find((s) => s.hash === cat);
   const isCameraValid = camera && streamCameras.includes(camera);
 
