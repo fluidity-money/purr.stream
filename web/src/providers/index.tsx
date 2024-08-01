497901836e7980b4d4c58353455ba141a5c6c098
ogous <oguzxan.ali@gmail.com>
2024-08-01 14:36:11 +0300

    fix: wrap useSearchParams suspense

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -2,12 +2,15 @@
 import { ThirdwebProvider } from "thirdweb/react";
 import ReactQueryProvider from "./reactQuery";
 import PermalinkController from "./permalink";
+import { Suspense } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThirdwebProvider>
       <ReactQueryProvider>
-        <PermalinkController />
+        <Suspense>
+          <PermalinkController />
+        </Suspense>
         {children}
       </ReactQueryProvider>
     </ThirdwebProvider>
