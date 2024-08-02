7b7a710bb3926e81fe17a6bc2d622d844a2429d6
ogous <oguzxan.ali@gmail.com>
2024-08-02 13:01:11 +0300

    fix: add style hooks to tailwind

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
